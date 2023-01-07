e4f8f15ce82000ba49f77571de6c85ab6f0e7a6c
ZhaoPeichen <1981336562@qq.com>
2023-01-07 21:18:20 +0800

    Grammar extends GrammarBase

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-class Grammar {
+class GrammarBase {
   private rules: Map<NT, Set<Sym[]>>
   start?: NT
 
@@ -7,6 +7,10 @@ class Grammar {
       return [nt, new Set(rhs)]
     }))
     this.start = ruleEntries[0]?.[0]
+  }
+
+  get alphabet() {
+    return this.calcAlphabet()
   }
 
   calcAlphabet() {
@@ -22,28 +26,65 @@ class Grammar {
     return set;
   }
 
+  get nonTerms() {
+    console.log('in base')
+    return this.calcNonTerms()
+  }
+
   calcNonTerms() {
     return new Set(this.rules.keys())
   }
 
-  calcTerms(
-    alphabet = this.calcAlphabet(),
-    nonTerms = this.calcNonTerms(),
-  ) {
-    const arr = [...alphabet].filter(sym => !nonTerms.has(sym))
+  isNonTerm(sym: Sym) {
+    return this.nonTerms.has(sym)
+  }
+
+  get terms() {
+    return this.calcTerms()
+  }
+
+  calcTerms() {
+    const arr = [...(this.alphabet)].filter(sym => !this.nonTerms.has(sym))
     return new Set(arr)
   }
 
-  calcFirst() {
+  isTerm(sym: Sym) {
+    return this.terms.has(sym)
+  }
 
+  calcFirst() {
+    const first = new Map<Sym, Set<Sym>>()
+  }
+}
+
+class Grammar extends GrammarBase {
+  _alphabet?: Set<Sym>
+  _nonTerms?: Set<Sym>
+  _terms?: Set<Sym>
+
+  get alphabet() {
+    return this._alphabet ??= this.calcAlphabet()
+  }
+
+  get nonTerms() {
+    console.log('in extend')
+    return this._nonTerms ??= this.calcNonTerms()
+  }
+
+  get terms() {
+    return this._terms ??= this.calcTerms()
+  }
+
+  protected invalidCache() {
+    this._terms = this._alphabet = this._nonTerms = undefined
   }
 }
 
 const grammar = new Grammar([
-  ['E', [['T', '+', 'E'], ['T', '-', 'E']]]
+  ['E', [['T', '+', 'E'], ['T', '-', 'E'], ['']]]
 ]);
 
-console.log(grammar.calcTerms())
+grammar.isNonTerm('E')
 
 type Sym = string
 type NT = Sym
