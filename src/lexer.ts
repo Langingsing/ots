95008609c24a055e3d455c1a67c10d9ee185d210
ZhaoPeichen <1981336562@qq.com>
2023-01-21 17:36:51 +0800

    add: lexer

---
+++ src/lexer.ts
@@ -0,0 +1,56 @@
+import type {Term} from "./types";
+
+export class Token {
+  constructor(
+    readonly type: Term,
+    readonly value: string,
+  ) {
+  }
+}
+
+export class Rule {
+  static readonly NUM: RulePair = ['num', /\d+/]
+  static readonly BLANK: RulePair = ['blank', /\s+/]
+
+  readonly regex: RegExp
+
+  constructor(
+    readonly type: Term,
+    pat: RegExp,
+  ) {
+    const regexStr = String(pat)
+    const regexBody = regexStr.substring(1, regexStr.length - 1)
+    this.regex = RegExp(`^${regexBody}`)
+  }
+}
+
+export type RulePair = Readonly<[Term, RegExp]>
+
+export class Lexer {
+  protected readonly rules: Rule[]
+
+  constructor(rules: readonly RulePair[]) {
+    this.rules = rules.map(rule => new Rule(...rule))
+  }
+
+  * parse(src: string) {
+    for (let i = 0; i < src.length;) {
+      let someMatched = false
+      for (const {type, regex} of this.rules) {
+        const m = src.substring(i).match(regex)
+        if (!m) {
+          continue
+        }
+        const [matched] = m
+        yield new Token(type, matched)
+        i += matched.length
+        someMatched = true
+        break
+      }
+      if (!someMatched) {
+        throw `unrecognized '${src[i]}' at index ${i}`
+      }
+    }
+    return [] as Token[]
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
 import {Grammar} from "./grammar.js"
+import {Lexer, Rule} from "./lexer.js"
 
 const grammar = new Grammar([
-  ['A', [['B', 'a'], ['']]],
-  ['B', [['A', 'E'], ['b']]],
-  ['E', [['T', '+', 'E'], ['T', '-', 'E'], ['']]]
-]);
+  ['Expr', [
+    ['Expr', '+', 'Term'],
+    ['Expr', '-', 'Term'],
+    ['Term'],
+  ]],
+  ['Term', [
+    ['Term', '*', 'Factor'],
+    ['Term', '/', 'Factor'],
+    ['Factor'],
+  ]],
+  ['Factor', [
+    ['(', 'Expr', ')'],
+    ['num']
+  ]],
+])
+// console.log(grammar.rules)
+// console.log('terms')
+// console.log(grammar.terms)
+// console.log('nts')
+// console.log(grammar.nonTerms)
+// console.log(grammar.epsilonProducers)
+// console.log(grammar.first)
+// console.log(grammar.follow)
+// console.log(grammar.calcSLRTable().toString())
 
-console.log(grammar.calcSLRTable())
+const str = '3 * (4 + 56)'
+console.log([...new Lexer([
+  Rule.NUM,
+  ['(', /\(/],
+  [')', /\)/],
+  ['+', /\+/],
+  ['-', /-/],
+  ['*', /\*/],
+  ['/', /\//],
+  Rule.BLANK,
+]).parse(str)])
